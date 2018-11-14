ac08d59bd0cf6876b0579b266eb217c435d80431
BenoitYnov <benoit.galmot@ynov.com>
2018-11-14 19:49:05 +0100

    Update main.js

--- ProjetNode/main.js
+++ ProjetNode/main.js
@@ -3,18 +3,24 @@
 const axios = require('axios');
 const program = require('commander')
 
+// Affichage menu
 program
     .option('-i, --info', "Plus d'info?")
     .option('-p, --pays', 'info France')
+    .option('-sp, --saisie_pays', "Saisissez le nom d'un pays pour des informations")
     .option('-c, --saisie_capitale', "Saisissez le nom d'une capitale pour des informations")
 
 program.parse(process.argv)
 if (program.info) {
     console.log("'Bienvenue sur l'Api des Pays")
+
+// Liste des Pays
 } else if (program.pays) {
     axios.get("https://restcountries.eu/rest/v2/all?fields=name;capital;currencies")
         .then(res => console.log(res.data))
         .catch(err => console.log(err.message))
+
+// Saisie de capitale
 } else if (program.saisie_capitale) {
     const inquirer = require('inquirer')
     inquirer.prompt([{
@@ -28,6 +34,16 @@ if (program.info) {
             console.log(answers.data)
         })
         .catch(err => console.log("Erreur, votre saisie n'est pas une capitale"))
+// Saisie Pays
+} else if (program.saisie_pays) {
+    const inquirer = requier('inquirer')
+    inquirer.prompt([{
+        type: 'input',
+        message: 'Choisisissez un pays',
+        name: 'pays'
+    }, ]).then((answersPays) => {
+        return axios.get("https: //restcountries.eu/rest/v2/name/" + answersPays.pays)
+    })
 } else {
     program.help()
 }
